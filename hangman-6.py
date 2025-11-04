4364e79232b0dbd0120dc96885f6ef428f77f0a1
Justin-Git4 <justinluppnow@gmail.com>
2025-11-04 08:01:04 +0200

    initial commit

---
+++ hangman-6.py
@@ -0,0 +1,115 @@
+import random
+
+HANGMAN = [
+r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+'''
+]
+
+
+print('\nWelcome to the man that hangs game !\n')
+
+lives = 6
+not_game_over = True
+
+words = ['ponytail', 'clouds', 'mountain']
+chosen_word = random.choice(words)
+
+guess_word = ''
+correct_letters = []
+
+for letter in chosen_word:
+    guess_word += '_'
+
+print(HANGMAN[lives])
+print(guess_word)
+print('\n')
+
+while not_game_over:
+    display = ''
+
+    if lives >= 2:
+        print(f"You have {lives} lifes left")
+    elif lives == 1:
+        print(f"You have {lives} life left")
+
+    user_letter = input('Select a letter : ').lower()
+
+    for letter in chosen_word:
+        if letter == user_letter :
+            display += letter
+            correct_letters.append(user_letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += '_'
+    
+    print('\n')
+    print(HANGMAN[lives])
+    print(display)
+    print('\n')
+
+    if user_letter not in chosen_word:
+        lives -= 1
+
+    if lives == 0:
+        print(f"Game over the correct word was -> {chosen_word}\n")
+        not_game_over = False
+
+    if '_' not in display:
+        print('You won\n')
+        not_game_over = False
